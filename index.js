33d940fe32d3bf2bee88d824f05f22d3bdb6d734
Kate Schlunz <schlunzk@gmail.com>
2017-11-12 20:56:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,10 +24,12 @@ class BoardMember {
   sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
-}
 
-class Ceo extends boardMember{
-  hireEmployee(){
-    return 'Welcome aboard'
+  class Ceo extends boardMember{
+    hireEmployee(){
+      return 'Welcome aboard'
+    }
   }
 }
+
+
